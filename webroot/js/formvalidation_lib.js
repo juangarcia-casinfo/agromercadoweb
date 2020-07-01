01de6a17e72d16c2c3a6e688492f922916ef791e
garykuipers <gary.kuipers@casinfosystems.com>
2020-07-01 14:01:45 -0700

    added notes to js validator filer

--- webroot/js/formvalidation_lib.js
+++ webroot/js/formvalidation_lib.js
@@ -14,12 +14,12 @@ function formValidator(rcvFormObj)
 	var errMsg 		= [];
 	var errCt 		= 0;
 	var modalHtml 	= "";
-	
+
 
 	if(rcvFormObj)
 	{
 		frmObj = document.forms[rcvFormObj.name];
-		
+
 		try
 		{
 			//Validating the fields
@@ -27,24 +27,24 @@ function formValidator(rcvFormObj)
 			{
 				//console.log(frmObj.elements[fi].id);
 				//console.log($('#'+frmObj.elements[fi].id).attr('class'));
-				
-				
+
+
 				//console.log(frmObj.elements[fi].id);
-				frmArr[frmObj.elements[fi].id] = frmObj.elements[fi].value;
-				
+				frmArr[frmObj.elements[fi].id] = frmObj.elements[fi].value; // this converts the KV pairs to a dict of keys and values
+
 				//Processing fields for validation
 				if($('#'+frmObj.elements[fi].id).attr('class')!=undefined)
 				{
 					var classList = $('#'+frmObj.elements[fi].id).attr('class').split(/\s+/);
 					var tmpErrMsg = "";
 
-					//Processing the classes					
+					//Processing the classes
 					for(vic=0;vic<classList.length;vic++)
 					{
 						//console.log('Processing Class: '+ classList[vic]);
 						tmpErrMsg = fieldValidator(frmObj.elements[fi].value, classList[vic]);
-						
-						
+
+
 						if(tmpErrMsg[0]=="1")
 						{
 							errMsg[frmObj.elements[fi].id] = tmpErrMsg[1];
@@ -53,45 +53,44 @@ function formValidator(rcvFormObj)
 					}
 				}
 			}
-			
-			
+
 			//Processing the results of the validations
 			if(errCt==0)
 			{
 				//Serialize the ansower in json format to be sent
-				//jsonData = $(frmObj).serializeArray();	
-				jsonData = JSON.stringify(frmArr);
-				
+				//jsonData = $(frmObj).serializeArray();
+				jsonData = JSON.stringify(frmArr);  // thgis converts the dict object into JSON string
+
 				console.log("Form data to be sent: vvv");
-				console.log(frmArr);	
-				console.log(jsonData);	
-				
+				console.log(frmArr);
+				console.log(jsonData);
+
 				sendData(jsonData);
 			}
 			else
 			{
 				//Shows the errors on the form
 				console.log(errMsg);
-				
+
 				//Modal HTML data
 				modalHtml = modalHtml + '<div id="dialog-message" title="Form Error Messages">';
-				
+
 				for(var tmpErrMsg in errMsg)
 				{
 					modalHtml = modalHtml + '<p>';
 					modalHtml = modalHtml + '<span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>';
 					modalHtml = modalHtml + tmpErrMsg + ': ' + errMsg[tmpErrMsg];
-					modalHtml = modalHtml + '</p>';				
+					modalHtml = modalHtml + '</p>';
 				}
-			
+
 				modalHtml = modalHtml + '</div>';
-				
+
 				$('#modal-container').html(modalHtml);
-				
+
 				$( "#dialog-message" ).dialog(
 				{
 				      modal: true,
-				      height: 400, 
+				      height: 400,
 				      width: 500,
 				      show:
 				      		{
@@ -105,7 +104,7 @@ function formValidator(rcvFormObj)
 				      			}
 				});
 			}
-			
+
 			return false;
 		}
 		catch(e)
@@ -114,7 +113,7 @@ function formValidator(rcvFormObj)
 			return false;
 		}
 	}
-	
+
 	return false;
 }
 
@@ -127,10 +126,10 @@ function fieldValidator(rcvFldData, rcvType)
 
 	var resultArr 		= ["", ""];
 	var typeOpts 		= rcvType.split("_");
-	
-	
-	var regexTime = /^(\d{2})+:+(\d{2})+:+(\d{2})$/;	
-	var regexDate = /^(\d{4})+-+(\d{2})+-+(\d{2})$/;	
+
+
+	var regexTime = /^(\d{2})+:+(\d{2})+:+(\d{2})$/;
+	var regexDate = /^(\d{4})+-+(\d{2})+-+(\d{2})$/;
 	var regexDt = /^(\d{4})+-+(\d{2})+-+(\d{2})+\s+(\d{2})+:+(\d{2})+:+(\d{2})$/;
 	var regexInt = /^(\d)+$/;
 	var regexDecimal = /^(\d)+(\.)?(\d)*$/;
@@ -139,11 +138,11 @@ function fieldValidator(rcvFldData, rcvType)
 	var regexWordsDigits = /^[\d\w]+$/;
 
 
-	
+
 	if(rcvFldData=="" || rcvFldData==undefined)
 	{
 		resultArr[0] = "1";
-		resultArr[1] = "The field is empty.";			
+		resultArr[1] = "The field is empty.";
 	}
 	else
 	{
@@ -153,28 +152,30 @@ function fieldValidator(rcvFldData, rcvType)
 				if(!regexInt.test(rcvFldData))
 				{
 					resultArr[0] = "1";
-					resultArr[1] = "The value is not an integer.";							
+					resultArr[1] = "The value is not an integer.";
 				}
 			break;
-			
+
 			case "decimal":
 				if(!regexDecimal.test(rcvFldData))
 				{
 					resultArr[0] = "1";
-					resultArr[1] = "The value is not a decimal.";							
+					resultArr[1] = "The value is not a decimal.";
 				}
 			break;
-			
+
 			case "percent":
+			    // remore pecentage sign if any
+                rcvFldData = rcvFldData.replace("%", "");
 				if(!regexDecimal.test(rcvFldData))
 				{
 					resultArr[0] = "1";
-					resultArr[1] = "The value is not an decimal.";							
+					resultArr[1] = "The value is not an decimal.";
 				}
 				else
 				{
 					rcvFldData = parseInt(rcvFldData, 10);
-				
+
 					if(rcvFldData<0 || rcvFldData > 100)
 					{
 						resultArr[0] = "1";
@@ -182,27 +183,27 @@ function fieldValidator(rcvFldData, rcvType)
 					}
 				}
 			break;
-			
+
 			case "date":
 				if(!regexDate.test(rcvFldData))
 				{
 					resultArr[0] = "1";
-					resultArr[1] = "The value is not an date.";							
-				}			
+					resultArr[1] = "The value is not an date.";
+				}
 			break;
-			
-			
+
+
 			case "datetime":
 				if(!regexDt.test(rcvFldData))
 				{
 					resultArr[0] = "1";
-					resultArr[1] = "The value is not datetime.";							
-				}			
+					resultArr[1] = "The value is not datetime.";
+				}
 			break;
-		}		
+		}
 	}
 
-	
+
 	return resultArr;
 }
 
@@ -212,7 +213,7 @@ function sendData(rcvDataStr)
 {
 	var translateData = rcvDataStr.replace(new RegExp(/\|+\|+DBLQT+\|+\|/, 'g'), '"');
 	console.log(translateData);
-	
+
 	$('#request_data').val(translateData);
 	$('#btn_send_data').click();
 }
